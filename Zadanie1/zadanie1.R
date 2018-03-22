79076c2e1bee80073327544cc089305557ff490c
Anna Pietrzak <195680@edu.p.lodz.pl>
2018-03-22 22:10:45 +0100

    algorytm bayesowski

--- Zadanie1/zadanie1.R
+++ Zadanie1/zadanie1.R
@@ -1,10 +1,21 @@
+#########ALGORYTM KNN#############
+
 #wczytanie danych
 data <- read.csv('C:/Users/Monia/Documents/Studia/1 semestr/4GL/Zadanie1/dane.csv', header = FALSE, sep = ' ')
+
+#przypisanie nazw kolumnom
+names(data) <- c("nauka_przedmiot", "l_powtorzen", "nauka_powiaz", "egz_powiaz", "egz_przedmiot", "wiedza")
+
+#wyswietlenie pierwszych pozycji zbioru
+head(data)
 
 #wyœwietlenie podsmumowania zbioru danych
 summary(data)
 
-#dziêki temu wywo³aniu mo¿na otrzymaæ powtarzalne rezultaty
+#wyswietlenie wykresu prezentuj¹cego poziom wiedzy w zaleznosci od wyniku egzaminow
+data %>% ggvis(~egz_powiaz, ~egz_przedmiot, fill = ~wiedza) %>% layer_points()
+
+#dziêki temu wywo³aniu mozna otrzymac powtarzalne rezultaty
 set.seed(1234)
 
 #wymieszanie danych ze zbioru
@@ -33,4 +44,22 @@ merge <- data.frame(data_pred, testLabels)
 
 names(merge) <- c("Przewidywane rezultaty", "Oczekiwane rezultaty")
 
+#wyswietlenie tabelki informujacej o liczbie pokrywajacych sie wynikow klasyfikacji
+CrossTable(x = testLabels, y = data_pred, prop.chisq=FALSE)
+
+#######ALGORYTM BAYESOWSKI#############
+
+#nalezy pobrac odpowiednie biblioteki
+library(e1071)
+library(caret)
+
+#przypisanie wartosci z kolumn 1-5 do x i klasy do y
+x=data[,-6]
+y=data$wiedza
+
+#stworzenie modelu, cross validation=10
+model = train(x, y, 'nb', trControl=trainControl(method='cv', number=10))
+
+#podsumowanie modelu
+model
 
