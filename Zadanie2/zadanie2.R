f6eb5731b46480c2b11ae7b6b2c048cd560671ae
mstobieniecka <mstobieniecka1@gmail.com>
2018-04-22 17:30:45 +0200

    Changed dbscan params

--- Zadanie2/zadanie2.R
+++ Zadanie2/zadanie2.R
@@ -3,6 +3,7 @@
 #wczytanie danych
 data <- read.csv('C:/Users/Monia/Documents/Studia/1 semestr/4GL/Zadanie1/dane.csv', header = FALSE, sep = ' ')
 data <- data[4: 5]
+
 #przypisanie nazw kolumnom
 names(data) <- c("egz_powiaz", "egz_przedmiot")
 set.seed(20)
@@ -15,10 +16,9 @@ library(dbscan)
 cluster.kmeans <- kmeans(data, 4)
 cluster.kmeans
 plot(data, pch = 19, col = cluster.kmeans$cluster)
-cluster.kmeans.groups <- cluster.kmeans$cluster
 
 ######## ALGORYTM DBSCAN ##############
 kNNdistplot(data)
-dist <- dist(data)
-cluster.dbcan <- dbscan(data, eps = 0.095, minPts = 5)
+cluster.dbcan <- dbscan(data, eps = 0.1, minPts = 5)
+cluster.dbcan
 plot(data, pch = 19, col = cluster.dbcan$cluster + 1L)
