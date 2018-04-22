149c0c461da0ce7564606a159f0228890f05ce44
mstobieniecka <mstobieniecka1@gmail.com>
2018-04-22 17:22:25 +0200

    Added dbscan algorithm

--- Zadanie2/zadanie2.R
+++ Zadanie2/zadanie2.R
@@ -1,18 +1,24 @@
 ######### PRZYGOTOWANIE DANYCH ##########
 
 #wczytanie danych
-data <- read.csv('C:/Users/Monia/Documents/Studia/1 semestr/4GL/Zadanie2/dane.csv', header = FALSE, sep = ' ')
-#data <- read.csv('D:/Studia - prace/2-stopien/4GL/repository/4GL/Zadanie2/dane.csv', header = FALSE, sep = ' ')
-
-data <- data[1: 5]
+data <- read.csv('C:/Users/Monia/Documents/Studia/1 semestr/4GL/Zadanie1/dane.csv', header = FALSE, sep = ' ')
+data <- data[4: 5]
 #przypisanie nazw kolumnom
-names(data) <- c("nauka_przedmiot", "l_powtorzen", "nauka_powiaz", "egz_powiaz", "egz_przedmiot")
+names(data) <- c("egz_powiaz", "egz_przedmiot")
+set.seed(20)
 
 ######## ZALADOWANIE PAKIETOW ##########
+install.packages("dbscan")
+library(dbscan)
 
 ######## ALGORYTM K-SREDNICH ###########
-set.seed(20)
 cluster.kmeans <- kmeans(data, 4)
 cluster.kmeans
 plot(data, pch = 19, col = cluster.kmeans$cluster)
 cluster.kmeans.groups <- cluster.kmeans$cluster
+
+######## ALGORYTM DBSCAN ##############
+kNNdistplot(data)
+dist <- dist(data)
+cluster.dbcan <- dbscan(data, eps = 0.095, minPts = 5)
+plot(data, pch = 19, col = cluster.dbcan$cluster + 1L)
