90fe41096d44fc782e2ed73c4a7a6a49488727a3
Anna Pietrzak <195680@edu.p.lodz.pl>
2018-04-22 15:44:53 +0200

    added path to dane.csv

--- Zadanie2/zadanie2.R
+++ Zadanie2/zadanie2.R
@@ -2,6 +2,8 @@
 
 #wczytanie danych
 data <- read.csv('C:/Users/Monia/Documents/Studia/1 semestr/4GL/Zadanie2/dane.csv', header = FALSE, sep = ' ')
+#data <- read.csv('D:/Studia - prace/2-stopien/4GL/repository/4GL/Zadanie2/dane.csv', header = FALSE, sep = ' ')
+
 data <- data[1: 5]
 #przypisanie nazw kolumnom
 names(data) <- c("nauka_przedmiot", "l_powtorzen", "nauka_powiaz", "egz_powiaz", "egz_przedmiot")
