f1afef8fd5912b12f7358e85c145d624db25665a
Anna Pietrzak <195680@edu.p.lodz.pl>
2018-04-26 22:43:09 +0200

    again changes in parameters values in dbscan..

--- Zadanie2/zadanie2.R
+++ Zadanie2/zadanie2.R
@@ -20,8 +20,8 @@ cluster.kmeans
 plot(data, pch = 19, col = cluster.kmeans$cluster)
 
 ######## ALGORYTM DBSCAN ##############
-kNNdistplot(data, k=9)
-cluster.dbcan <- dbscan(data, eps = 0.095, minPts = 9)
+kNNdistplot(data, k=4)
+cluster.dbcan <- dbscan(data, eps = 0.095, minPts = 4)
 cluster.dbcan
 plot(data, pch = 19, col = cluster.dbcan$cluster + 1L)
 
