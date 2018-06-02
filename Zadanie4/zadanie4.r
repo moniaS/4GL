1158db792ce19e8cac6c54a5516d09eeb3edb451
mstobieniecka <mstobieniecka1@gmail.com>
2018-06-02 10:10:56 +0200

    Added clustering for gas dataset

--- Zadanie4/zadanie4.r
+++ Zadanie4/zadanie4.r
@@ -9,3 +9,24 @@
 data.gaz <- read.csv('D:/Studia - prace/2-stopien/4GL/repository/4GL/Zadanie4/gaz.csv', header = TRUE, sep = ',')
 data.inflacja <- read.csv('D:/Studia - prace/2-stopien/4GL/repository/4GL/Zadanie4/inflacja.csv', header = TRUE, sep = ',')
 
+data.gaz$Price <- as.numeric(as.character(data.gaz$Price))
+data.gaz$Date <- as.Date(as.character(data.gaz$Date))
+
+data.gaz.timeseries <- as.xts(data.gaz[,-1], order.by = data.gaz$Date)
+
+######## ZA£ADOWANIE PAKIETÓW ##########
+
+#za³adowanie biblioteki do grupowania
+install.packages("dtwclust")
+library(dtwclust)
+
+#za³adowanie biblioteki do time series
+install.packages("xts")
+library(xts)
+
+######## GRUPOWANIE ##########
+
+data.gaz.clust <- tsclust(data.gaz.timeseries, type = "partitional", k = 10L, 
+              distance = "euclidean", centroid = "pam", seed = 3247L, trace = TRUE)
+
+plot(data.gaz.clust)
