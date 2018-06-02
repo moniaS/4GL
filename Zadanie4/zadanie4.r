840ad2524d8af7a080f9af0b53a0ecd0fa05f25c
mstobieniecka <mstobieniecka1@gmail.com>
2018-06-02 20:22:48 +0200

    Added plots for grouping

--- Zadanie4/zadanie4.r
+++ Zadanie4/zadanie4.r
@@ -13,7 +13,7 @@ data.gaz$Price <- as.numeric(as.character(data.gaz$Price))
 data.gaz$Date <- as.Date(as.character(data.gaz$Date))
 data.gaz.timeseries <- as.xts(data.gaz[,-1], order.by = data.gaz$Date)
 
-data.inflacja <- data.inflacja[1:200,]
+data.inflacja <- data.inflacja[1:300,]
 data.inflacja$Value <- as.numeric(as.character(data.inflacja$Value))
 data.inflacja$Date <- as.Date(as.character(data.inflacja$Date))
 data.inflacja.timeseries <- as.xts(data.inflacja[,-1], order.by = data.inflacja$Date)
@@ -29,20 +29,26 @@ install.packages("xts")
 library(xts)
 
 ######## GRUPOWANIE ZBIORU NR 1 ##########
-
 data.gaz.clust <- tsclust(data.gaz.timeseries, k = 4, type = "hierarchical", distance = "DTW",
                                    seed = 3247)
+plot(data.gaz.timeseries, xlab="Data", ylab="Cena")
 
-plot(data.gaz.clust@cluster)
+plot(x = data.gaz$Date, y = data.gaz.clust@cldist, xlab="Data", ylab="Odleg³oœæ")
+
+plot(data.gaz, pch = 19, col = data.gaz.clust@cluster, xlab="Data", ylab="Cena")
 
 plot(data.gaz.clust, clus = seq_len(x@k), plot = TRUE, type = "dendrogram")
 
 ######## GRUPOWANIE ZBIORU NR 2 ##########
 
-data.inflacja.clust <- tsclust(data.inflacja.timeseries, k = 3, type = "hierarchical", distance = "DTW",
+data.inflacja.clust <- tsclust(data.inflacja.timeseries, k = 5, type = "hierarchical", distance = "DTW",
                           seed = 3247)
 
-plot(data.inflacja.clust@cluster)
+plot(data.inflacja.timeseries, xlab="Data", ylab="Wartoœæ", main="Inflacja")
+
+plot(x = data.inflacja$Date, y = data.inflacja.clust@cldist, xlab="Data", ylab="Odleg³oœæ")
+
+plot(data.inflacja, pch = 19, col = data.inflacja.clust@cluster, xlab="Data", ylab="Wartoœæ")
 
 plot(data.inflacja.clust, clus = seq_len(x@k), plot = TRUE, type = "dendrogram")
 
@@ -66,5 +72,3 @@ data.inflacja <- read.csv('D:/Studia - prace/2-stopien/4GL/repository/4GL/Zadani
 data.inflacja$Date <- as.POSIXct(strptime(data.inflacja$Date, "%Y-%m-%d", tz = "UTC"))
 anomalie.inflacja <- AnomalyDetectionTs(data.inflacja, max_anoms=0.05, direction='both', plot=TRUE)
 anomalie.inflacja
-
-
